7e2e26892f46123a5740dd3434b9100d7f50ff78
Lulu Sheng <lulu.ruilu.sheng@gmail.com>
2025-03-27 16:36:19 -0400

    Add span

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,8 @@ class Book < FrozenRecord::Base
   self.base_path = 'db/frozen'
 
   def process
-    sleep(rand(0.0..2.0)) # TODO: remove this before pushing to GitHub!
+    Sentry.with_child_span(op: :process_book) do
+      sleep(rand(0.0..2.0)) # TODO: remove this before pushing to GitHub!
+    end
   end
 end
