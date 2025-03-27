e1b0c4b4d83679afc63fbe2031776dedf8a020c1
Lulu Sheng <lulu.ruilu.sheng@gmail.com>
2025-03-27 11:19:38 -0400

    Allow dynamic URL

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module FrozenApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << /^[a-zA-Z0-9.-]+$/
   end
 end
